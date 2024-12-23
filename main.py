a75ce80e46d19f3890694df5057c2d0202a80a94
jdiegoms <jdiegoms@yahoo.com>
2024-12-23 17:49:43 +1100

    made circleshape

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
