3ef69ef6977be7ebb31b141b784bead83f48686c
jdiegoms <jdiegoms@yahoo.com>
2025-01-06 15:09:23 +1300

    created containers

--- main.py
+++ main.py
@@ -11,19 +11,30 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for x in updatable:
+            x.update(dt)
+
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
+
+        for x in drawable:
+            x.draw(screen)
+        
         pygame.display.flip()
         
 
         dt = clock.tick(60) / 1000
-    
-    
+
+
 
 if __name__ == "__main__":
     main()
